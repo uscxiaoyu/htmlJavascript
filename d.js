12462c40a28e1e721e166460d0e780058fb9966a
uscxiaoyu <jxndtbxiaoyu@163.com>
2019-04-18 14:39:52 +0800

    win

--- d.js
+++ d.js
@@ -1,13 +1,20 @@
+'use strict';
 function rand_n(n) {
     let a = Math.random();
     for (let i=1; i<=n; i++){
-        if ((i - 1)/n <= a < i/n) {
+        if ((i - 1)/n <= a && a < i/n) {
             return i;
-        };
+        }
     };
 };
 
-for (let j=1; j<10; j++) {
-    document.write(rand_n(5));
-    document.write("<br>")
-}
+var a_array = [];
+var n = 20;
+for (let j=1; j<2*n; j++) {
+    var b = rand_n(n);
+    a_array.push(b);
+};
+
+alert(`运行成功！
+产生${n}个随机整数: ${a_array}`);
+document.write(a_array);
